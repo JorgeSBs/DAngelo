7e6459c0c98e1817743bf4908886b0e70953849f
JorgeSBs <jorgearmandoserranobriseno@gmail.com>
2025-03-06 14:41:15 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,65 +1,67 @@
+const CACHE_VERSION = 'v2';
+const CACHE_NAME = `${CACHE_VERSION}-dangelo-cache`;
 const ASSETS = [
   // HTML
-  '/',
-  '/index.html',
-  '/about.html',
-  '/appointment.html',
-  '/contact.html',
-  '/feature.html',
-  '/service.html',
-  '/team.html',
-  '/testimonial.html',
+  '/DAngelo/',
+  '/DAngelo/index.html',
+  '/DAngelo/about.html',
+  '/DAngelo/appointment.html',
+  '/DAngelo/contact.html',
+  '/DAngelo/feature.html',
+  '/DAngelo/service.html',
+  '/DAngelo/team.html',
+  '/DAngelo/testimonial.html',
+
   // CSS
-  '/css/bootstrap.min.css',
-  '/css/style.css',
-  '/lib/animate/animate.min.css', // Asegúrate de que exista
+  '/DAngelo/css/bootstrap.min.css',
+  '/DAngelo/css/style.css',
+  '/DAngelo/lib/animate/animate.min.css',
+
   // JS
-  '/js/main.js',
-  '/lib/counterup/counterup.min.js',
-  '/lib/easing/easing.min.js',
-  '/lib/owlcarousel/owl.carousel.min.js',
-  '/lib/tempusdominus/js/tempusdominus-bootstrap-4.js',
-  '/lib/waypoints/waypoints.min.js',
-  '/lib/wow/wow.min.js',
-  // Imágenes (verifica rutas)
-  '/img/logo1.jpg',
-  '/img/logo2.jpg',
-  // ... resto de imágenes
-  '/img/about-1.jpg',
-  '/img/about-2.jpg',
-  '/img/carousel-1.jpg',
-  '/img/carousel-2.jpg',
-  '/img/carousel-3.jpg',
-  '/img/feature.jpg',
-  '/img/header-page.jpg',
-  '/img/team-1.jpg',
-  '/img/team-2.jpg',
-  '/img/team-3.jpg',
-  '/img/team-4.jpg',
-  '/img/testimonial-1.jpg',
-  '/img/testimonial-2.jpg',
-  '/img/testimonial-3.jpg',
-  // Manifest
-  '/manifest.json',
-  '/clinic-website-template.jpg'
+  '/DAngelo/js/main.js',
+  '/DAngelo/lib/counterup/counterup.min.js',
+  '/DAngelo/lib/easing/easing.min.js',
+  '/DAngelo/lib/owlcarousel/owl.carousel.min.js',
+  '/DAngelo/lib/tempusdominus/js/tempusdominus-bootstrap-4.js',
+  '/DAngelo/lib/waypoints/waypoints.min.js',
+  '/DAngelo/lib/wow/wow.min.js',
+
+  // Imágenes
+  '/DAngelo/img/logo1.jpg',
+  '/DAngelo/img/logo2.jpg',
+  '/DAngelo/img/about-1.jpg',
+  '/DAngelo/img/about-2.jpg',
+  '/DAngelo/img/carousel-1.jpg',
+  '/DAngelo/img/carousel-2.jpg',
+  '/DAngelo/img/carousel-3.jpg',
+  '/DAngelo/img/feature.jpg',
+  '/DAngelo/img/header-page.jpg',
+  '/DAngelo/img/team-1.jpg',
+  '/DAngelo/img/team-2.jpg',
+  '/DAngelo/img/team-3.jpg',
+  '/DAngelo/img/team-4.jpg',
+  '/DAngelo/img/testimonial-1.jpg',
+  '/DAngelo/img/testimonial-2.jpg',
+  '/DAngelo/img/testimonial-3.jpg',
+
+  // Manifest e ícono
+  '/DAngelo/manifest.json',
+  '/DAngelo/img/icon-192x192.png',
+  '/DAngelo/img/icon-512x512.png'
 ];
 
-// Instalación con debug
+// Instalar Service Worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return Promise.all(
-          ASSETS.map(asset => {
-            return cache.add(asset).catch(error => {
-              console.error(`Fallo en ${asset}:`, error);
-              throw error;
-            });
-          })
-        );
-      })
-      .catch(error => {
-        console.error('Error durante la instalación:', error);
-      })
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch de recursos
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
